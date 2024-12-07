6d6f441ff83ac9fe7ed3dbee859b7e6432ae2d50
Joseph Chou <joe.nicu@gmail.com>
2024-12-07 16:11:55 -0500

    Day 7 recursive solution, cleaned up (mostly)

--- day7_recursive.jl
+++ day7_recursive.jl
@@ -1,57 +1,24 @@
-using IterTools # for product()
-
-function test_line(target, elements, operations)
-    for ops in product(fill( operations, length(elements) - 1)...)
-        # generate iterator across all possible operations between elements
-        result = elements[1]
-        for i in 1:(length(elements) - 1)
-            if ops[i] == '+'
-                result +=  elements[i+1]
-            elseif ops[i] == '*'
-                result *=  elements[i+1]
-            elseif ops[i] == '|'
-                result = parse(Int, string(result) * string(elements[i+1]))
-            else
-                println("ERROR")
-            end
-            if result > target
-                break
-                # this is still not fast, because will still check all the other ops sequences that are initially identical
-                # recursive procedure would be MUCH faster, because would not further explore down trees that failed earlier
-            end
-        end
-        if result == target
-            return target
-        end
-    end
-    return 0
-end
-
 function test_recurs(target, elements, running, i, operator; is_part2 = false)
-    if i == length(elements)
-        return running == target
-    else
-        if operator == '+'
-            running += elements[i+1]
-        elseif operator == '*'
-            running *= elements[i+1]
-        elseif operator == '|'
-            running = parse(Int, string(running) * string(elements[i+1]))
-        else
-            return nothing
-        end
-        if running > target
-            return false
-        elseif !is_part2
-            return (test_recurs(target, elements, running, i+1, '+'; is_part2)) || (test_recurs(target, elements, running, i+1, '*'; is_part2))
-        else
-            return (test_recurs(target, elements, running, i+1, '+'; is_part2)) || (test_recurs(target, elements, running, i+1, '*'; is_part2)) || (test_recurs(target, elements, running, i+1, '|'; is_part2))
-        end
+    if operator == '+'
+        running += elements[i+1]
+    elseif operator == '*'
+        running *= elements[i+1]
+    else # operator == '|'; should probably do this with math and not strings
+        running = parse(Int, string(running) * string(elements[i+1]))
     end
-end
 
-function test_init(target, elements; is_part2 = false)
-    test_recurs(target, elements, 0, 0, '+'; is_part2 = is_part2)
+    if running > target
+        return false
+    elseif i == length(elements) - 1
+        return running == target
+    elseif !is_part2
+        return  (test_recurs(target, elements, running, i+1, '+'; is_part2)) ||
+                (test_recurs(target, elements, running, i+1, '*'; is_part2))
+    else # for part 2
+        return  (test_recurs(target, elements, running, i+1, '+'; is_part2)) ||
+                (test_recurs(target, elements, running, i+1, '*'; is_part2)) ||
+                (test_recurs(target, elements, running, i+1, '|'; is_part2))
+    end
 end
 
 function test_file(file)
@@ -61,16 +28,14 @@ function test_file(file)
         m = match(r"(.*): (.*)", line)
         target = parse(Int, m.captures[1])
         elements = parse.(Int, split(m.captures[2], " "))
-        part1 += test_init(target, elements; is_part2 = false) ? target : 0
-        part2 += test_init(target, elements; is_part2 = true) ? target : 0
+        # Initial recusion call: will '+' the first element to a running total of 0 to start
+        part1 += test_recurs(target, elements, 0, 0, '+'; is_part2 = false) ? target : 0
+        part2 += test_recurs(target, elements, 0, 0, '+'; is_part2 = true) ? target : 0
+
     end
     return (part1, part2)
 end
 
-# file = "data/day7test.txt"
-file = "data/day7.txt"
-
-(part1, part2) = test_file(file)
-
+(part1, part2) = test_file("data/day7.txt")
 part1 # Part 1: 14711933466277
 part2 # Part 2: 286580387663654
